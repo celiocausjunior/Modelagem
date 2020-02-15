98cc8eda9ccdfefa2f5d3cf1167e7331d7853d68
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-15 20:26:41 -0300

    Estado e Cidade

--- src/main/java/com/celiocausjr/ModelagemConceitualApplication.java
+++ src/main/java/com/celiocausjr/ModelagemConceitualApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.celiocausjr.domain.Categoria;
+import com.celiocausjr.domain.Cidade;
+import com.celiocausjr.domain.Estado;
 import com.celiocausjr.domain.Produto;
 import com.celiocausjr.repositories.CategoriaRepository;
+import com.celiocausjr.repositories.CidadeRepository;
+import com.celiocausjr.repositories.EstadoRepository;
 import com.celiocausjr.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -42,9 +52,21 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/celiocausjr/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.celiocausjr.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.celiocausjr.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
