416c557d038ebc4c3786f0cb7c09326723204e0d
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-14 22:17:02 -0300

    Produto Domain, Instanciacao de Produto e Associacoes

---
+++ src/main/java/com/celiocausjr/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.celiocausjr.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.celiocausjr.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
