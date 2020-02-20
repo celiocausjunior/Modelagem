63e356226c841835958a20361246ab46c272aced
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-19 22:38:58 -0300

    Delete e Data Integrity Exception

--- src/main/java/com/celiocausjr/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/celiocausjr/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.celiocausjr.servicies.exceptions.ConstraintException;
 import com.celiocausjr.servicies.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,4 +19,11 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	@ExceptionHandler(ConstraintException.class)
+	public ResponseEntity<StandardError> constraintException(ConstraintException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	
 }
--- src/main/java/com/celiocausjr/servicies/CategoriaService.java
+++ src/main/java/com/celiocausjr/servicies/CategoriaService.java
@@ -3,10 +3,12 @@ package com.celiocausjr.servicies;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.celiocausjr.domain.Categoria;
 import com.celiocausjr.repositories.CategoriaRepository;
+import com.celiocausjr.servicies.exceptions.ConstraintException;
 import com.celiocausjr.servicies.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new ConstraintException("Não é possível excluir categorias com produtos");
+		}
 	}
 }
