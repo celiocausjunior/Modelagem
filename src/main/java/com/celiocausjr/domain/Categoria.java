339fa2a938a95ce954cb2789269abc88132fccd7
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-21 21:49:12 -0300

    Correções

--- src/main/java/com/celiocausjr/domain/Categoria.java
+++ src/main/java/com/celiocausjr/domain/Categoria.java
@@ -17,7 +17,7 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String nome;
 
 	
 	@ManyToMany(mappedBy = "categorias")
@@ -26,10 +26,10 @@ public class Categoria implements Serializable {
 	public Categoria() {
 	}
 
-	public Categoria(Integer id, String name) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 	}
 
 	public Integer getId() {
@@ -40,12 +40,12 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public List<Produto> getProdutos() {
