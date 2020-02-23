1b6aebe28b5dd5c0be662fdf5048a9e04892e130
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-23 17:16:06 -0300

    Validação customizada - email não repetido na atualizacao do cliente

--- src/main/java/com/celiocausjr/dto/ClienteDTO.java
+++ src/main/java/com/celiocausjr/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.celiocausjr.domain.Cliente;
+import com.celiocausjr.servicies.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
