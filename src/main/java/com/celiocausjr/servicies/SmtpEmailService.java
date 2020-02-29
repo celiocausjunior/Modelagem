0f48dd8a2e1e25bea9191f7179fd97958eb0c6c4
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-28 22:16:43 -0300

    Email Html

--- src/main/java/com/celiocausjr/servicies/SmtpEmailService.java
+++ src/main/java/com/celiocausjr/servicies/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.celiocausjr.servicies;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -15,10 +18,23 @@ public class SmtpEmailService extends AbstractEmailService {
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email....");
 		mailSender.send(msg);
+		LOG.info("Email enviado!");
+		
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado!");
 		
 	}
