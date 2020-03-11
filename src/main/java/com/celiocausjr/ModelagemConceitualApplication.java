914a554759f8827f35a8d780ccab114363229a8f
Celio Caus Junior <celiocausjunior@gmail.com>
2020-03-10 23:36:41 -0300

    Salvando arquivo no S3

--- src/main/java/com/celiocausjr/ModelagemConceitualApplication.java
+++ src/main/java/com/celiocausjr/ModelagemConceitualApplication.java
@@ -1,12 +1,19 @@
 package com.celiocausjr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.celiocausjr.servicies.S3Service;
+
 @SpringBootApplication
 public class ModelagemConceitualApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
 	}
@@ -14,5 +21,7 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("C:\\Users\\CASA\\iCloudDrive\\SpringBoot & Ionic\\diagrama.png");
+		
 	}
 }
